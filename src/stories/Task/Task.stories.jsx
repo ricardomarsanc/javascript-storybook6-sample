2b5d59d936b958206ace77bfb126b9b028045ca1
Ricardo Martín <ricardomartin@playtomic.io>
2022-11-15 08:17:07 +0100

    feat: added ellipsis to task long text

--- src/stories/Task/Task.stories.jsx
+++ src/stories/Task/Task.stories.jsx
@@ -32,3 +32,12 @@ Archived.args = {
     state: 'TASK_ARCHIVED'
   }
 }
+
+export const LongTitle = Template.bind({})
+LongTitle.args = {
+  task: {
+    ...Default.args.task,
+    title:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo accusamus sed iste nemo exercitationem unde amet vero possimus totam beatae non odio quidem voluptatem dolores, voluptas, numquam assumenda, dolore quod!'
+  }
+}
